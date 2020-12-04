20f4680a5c8f48ed67e51c0b448869cd175958a7
Milan Hauth <milahu@gmail.com>
2020-12-04 14:57:16 +0100

    handle negative depth

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub fn parse_sexpr(source: &str, indent_step: &str) -> Vec<String> {
-    let mut depth: usize = 0;
+    let mut depth: i32 = 0;
     let mut start: usize = 0;
     let mut result: Vec<String> = Vec::new();
     let mut in_name: bool;
@@ -14,7 +14,8 @@ pub fn parse_sexpr(source: &str, indent_step: &str) -> Vec<String> {
         // handle start/end of name
         if in_name && !last_in_name { start = i }
         else if !in_name && last_in_name {
-            let mut s: String = indent_step.repeat(depth - 1);
+            let num_indent: usize = if depth >= 1 { (depth - 1) as usize } else { 0 };
+            let mut s: String = indent_step.repeat(num_indent);
             s.push_str(&source[start..i]);
             result.push(s);
         }
@@ -24,7 +25,8 @@ pub fn parse_sexpr(source: &str, indent_step: &str) -> Vec<String> {
     }
     if depth != 0 {
         // TODO throw exception
-        println!("ERROR parse_sexpr: braces mismatch. depth {} should be zero", depth);
+        println!("ERROR parse_sexpr: braces mismatch. found {} extra {}-braces",
+            depth.abs(), if depth > 0 { "open" } else { "close" });
     }
     return result;
 }
